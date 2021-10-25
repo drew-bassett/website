67bc41ad32e8270a5730fb42b08ffa2a9d9dfef1
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2021-10-25 19:44:39 +0100

    css

--- resources/views/coe.blade.php
+++ resources/views/coe.blade.php
@@ -2,7 +2,7 @@
 
 @section ('content')
     <h1>Centre of Excellence Certificates</h1>
-    <p>In my spare time, I enjoy doing courses on <a href="https://www.centreofexcellence.com/" target="_blank">Centre of Excellence</a>.</p>
+    <p>In my spare time, I enjoy doing courses through <a href="https://www.centreofexcellence.com/" target="_blank">Centre of Excellence</a>.</p>
     <img src="images/egyptology.jpg" alt="Egyptology" width="263" height="374">
     <img src="images/criminology.jpg" alt="Criminology"width="263" height="374">
     <img src="images/proofreading.jpg" alt="Proof Reading" width="263" height="374">
