bafb7d7b25c47b1263daed1580f627d408b2fa3f
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2021-10-25 19:28:17 +0100

    layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,23 +4,19 @@
     <h1>Miss Drew Baxter</h1>
     <img src="{{ url('images/drew.jpg') }}" alt="Drew">
     <h2>🎓 Education</h2>
-    <div class="text">
-        <p>
-            <b>▸</b> Studying for MSc Computer Science with Cyber Security at University of York<br/>
-            <b>▸</b> BSc Mathematics First-class Honours from Newcastle University<br />
-            <b>▸</b> A Levels in Mathematics, Further Mathematics, and Physics<br />
-        </p>
-    </div>
+    <p>
+        <b>▸</b> Studying for MSc Computer Science with Cyber Security at University of York<br/>
+        <b>▸</b> BSc Mathematics First-class Honours from Newcastle University<br />
+        <b>▸</b> A Levels in Mathematics, Further Mathematics, and Physics<br />
+    </p>
     <h2>💻 Employment</h2>
-    <div class="text">
-        <p>
-            <b>▸</b> IT Manager since October 2021 at Redu Group<br/>
-            <b>▸</b> System Support Executive for 11 months at Redu Group<br/>
-            <b>▸</b> Sales Order Processor for 10 Years 3 Months at Redu Group
-        </p>
-    </div>
+    <p>
+        <b>▸</b> IT Manager since October 2021 at Redu Group<br/>
+        <b>▸</b> System Support Executive for 11 months at Redu Group<br/>
+        <b>▸</b> Sales Order Processor for 10 Years 3 Months at Redu Group
+    </p>
     <h2>🏆 Certifications</h2><br/>
-    <div data-iframe-width="300" data-iframe-height="200" data-share-badge-id="ce51ce4c-81f4-4fa1-9079-16b4761b663e" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
-    <div data-iframe-width="300" data-iframe-height="200" data-share-badge-id="434150f6-a0ec-405b-8edf-38a9fe15dae4" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
-    <div data-iframe-width="300" data-iframe-height="200" data-share-badge-id="15a6a67e-f6ed-497a-8f6a-9b2dc799f522" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
+    <div data-iframe-width="300" data-iframe-height="200" data-share-badge-id="ce51ce4c-81f4-4fa1-9079-16b4761b663e" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="https://cdn.credly.com/assets/utilities/embed.js"></script>
+    <div data-iframe-width="300" data-iframe-height="200" data-share-badge-id="434150f6-a0ec-405b-8edf-38a9fe15dae4" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="https://cdn.credly.com/assets/utilities/embed.js"></script>
+    <div data-iframe-width="300" data-iframe-height="200" data-share-badge-id="15a6a67e-f6ed-497a-8f6a-9b2dc799f522" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="https://cdn.credly.com/assets/utilities/embed.js"></script>
 @endsection
