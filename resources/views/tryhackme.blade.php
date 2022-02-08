8873e474dce5bae612c873d9f34427892c2132d7
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2022-02-08 18:12:25 +0000

    Update tryhackme.blade.php

--- resources/views/tryhackme.blade.php
+++ resources/views/tryhackme.blade.php
@@ -3,7 +3,8 @@
 @section ('content')
     <h1>TryHackMe</h1>
     <p>
-       <a href="https://tryhackme.com/" target="_blank">TryHackMe</a> is a great resource for cyber security training. The certificates I have earned are below.
+       <a href="https://tryhackme.com/" target="_blank">TryHackMe</a> is a great resource for cyber security training. The certificates I have earned are below.<br/>
+       <center><script src="https://tryhackme.com/badge/785231"></script></center>
     </p>
 	<img src="images/THM-December2021.png" alt="Christmas 2021" height="331" width="546">
 @endsection
