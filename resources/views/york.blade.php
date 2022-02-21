e10f53e337d2a59a3381e0b96a98fef9a2e4103a
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2022-02-21 19:29:48 +0000

    Update york.blade.php

--- resources/views/york.blade.php
+++ resources/views/york.blade.php
@@ -11,8 +11,8 @@
         <img src="/images/shopping.gif" alt="Shopping List Programme" width="75%" height="75%"></p>
 		
 		<h2>Module 2 - Research Methods</h2><p>
-		The second module developed my academic writing and research skills. I was introduced to a lot of terminology and theory regarding research methods and methodologies.</p>
+		The second module developed my academic writing and research skills, allowing me to confidentally use reputable sources to produce argumentative writing. I was also introduced to a lot of terminology and theory regarding research methods and methodologies. </p>
 		
 		<h2>Module 3 - Cyber Security Threats</h2><p>
-		I am currently studying this module.</p>
+		I am currently studying this module. I produced a risk analysis and treatment plan based around cyber security threats for a theoretical organisation.</p>
 @endsection
