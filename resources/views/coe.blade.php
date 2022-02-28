b0fad75a9f47f8cf86b4278ee349f0d0f6f51448
missdrewbaxter <75068256+missdrewbaxter@users.noreply.github.com>
2022-02-28 19:04:46 +0000

    broken image

--- resources/views/coe.blade.php
+++ resources/views/coe.blade.php
@@ -5,5 +5,5 @@
     <p>One of my interests is doing history and general interest courses through <a href="https://www.centreofexcellence.com/" target="_blank">Centre of Excellence</a>. The certificates for my completed courses are below, but I also have the following on my to-do list: Introduction to the Roman Empire Diploma; Maya and Aztec History Diploma; and Conspiracy Theories Diploma.</p>
     <img class="cert" s src="images/Egyptology.png" alt="Egyptology">
     <img class="cert" s src="images/Criminology.png" alt="Criminology">
-    <img class="cert" s src="images/Proofreading.jpg" alt="Proofreading and Editing">
+    <img class="cert" s src="images/Proofreading.png" alt="Proofreading and Editing">
 @endsection
--- resources/views/cpd.blade.php
+++ resources/views/cpd.blade.php
@@ -2,7 +2,7 @@
 
 @section ('content')
     <h1>CPD Certificates</h1>
-    <p>Below are come of the Continuing Personal Development certificates I have earned, categoried by the provider. <a href="/coe">Centre of Excellence</a> courses are also classed as CPD activities however the below are more relevant to my IT career.</p>
+    <p>Below are come of the Continuing Personal Development certificates I have earned, categoried by the provider. <a href="/coe">Centre of Excellence</a> courses are also classed as CPD activities, however the below are more relevant to my IT career.</p>
 	
 	<h2>Staff Skills Training</h2>
     <img class="cert" s src="images/helpdesk_support_analyst.png" alt="Helpdesk Support Analyst">
