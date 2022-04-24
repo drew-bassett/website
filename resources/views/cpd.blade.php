6a044381a12b324eac6b2e7a61d296b931f1fe54
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2022-04-24 16:55:26 +0100

    Update cpd.blade.php

--- resources/views/cpd.blade.php
+++ resources/views/cpd.blade.php
@@ -5,6 +5,7 @@
     <p>Below are come of the Continuing Personal Development certificates I have earned, categoried by the provider. <a href="/coe">Centre of Excellence</a> courses are also classed as CPD activities, however the below are more relevant to my IT career.</p>
 	
 	<h2>Staff Skills Training</h2>
-    <img class="cert" s src="images/helpdesk_support_analyst.png" alt="Helpdesk Support Analyst">
-    <img class="cert" s src="images/cables.png" alt="Cable and Wire Management Awareness">
+    <img class="cert" src="images/helpdesk_support_analyst.png" alt="Helpdesk Support Analyst">
+    <img class="cert" src="images/cables.png" alt="Cable and Wire Management Awareness">
+    <img class="cert" src="images/data_protection.png" alt="Data Protection">
 @endsection
