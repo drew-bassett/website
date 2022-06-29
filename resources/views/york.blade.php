f5de1b12879a499c125cea51d620dd24fdc5b75d
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2022-06-29 19:51:39 +0100

    Update york.blade.php

--- resources/views/york.blade.php
+++ resources/views/york.blade.php
@@ -10,15 +10,18 @@
         This module was in two halves: an introduction to Java and OOP, and then theory on algorithms and data structures. I made a simple shopping list program in Java; the below GIF demonstrates the program as well as some error handling. The source code is available on my <a href="https://github.com/missdrewbaxter/ShoppingList" target="_blank">GitHub</a>. <br/><br/>
         <img src="/images/shopping.gif" alt="Shopping List Programme" width="75%" height="75%"></p>
 		
-		<h2>Module 2 - Research Methods</h2><p>
-		The second module developed my academic writing and research skills, allowing me to confidentally use reputable sources to produce argumentative writing. I was also introduced to a lot of terminology and theory regarding research methods and methodologies. This was my first acaedmic essay since GCSEs, and I got a distinction for it!</p>
+	<h2>Module 2 - Research Methods</h2><p>
+	The second module developed my academic writing and research skills, allowing me to confidentally use reputable sources to produce argumentative writing. I was also introduced to a lot of terminology and theory regarding research methods and methodologies. This was my first acaedmic essay since GCSEs, and I got a distinction for it!</p>
 		
-		<h2>Module 3 - Cyber Security Threats</h2><p>
-		After an introduction to common cyber security threats, with a focus on web and network security, I produced a risk analysis and treatment plan for a theoretical organisation.</p>
+	<h2>Module 3 - Cyber Security Threats</h2><p>
+	After an introduction to common cyber security threats, with a focus on web and network security, I produced a risk analysis and treatment plan for a theoretical organisation.</p>
 		
-		<h2>Module 4 - Advanced Programming</h2><p>
-		Expanding on what I learned in module 1, I was introduced to Python and a few advanced programming concepts. My assignment was to create an application (with a GUI), that would parse user-uploaded CSV files, clean and perform data analysis on them, resulting in some summary statistics and graphs for the user.</p>
+	<h2>Module 4 - Advanced Programming</h2><p>
+	Expanding on what I learned in module 1, I was introduced to Python and a few advanced programming concepts. My assignment was to create an application (with a GUI), that would parse user-uploaded CSV files, clean and perform data analysis on them, resulting in some summary statistics and graphs for the user.</p>
 		
-		<h2>Module 5 - Artificial Intelligence and Machine Learning</h2><p>
-		Coming soon...</p>
+	<h2>Module 5 - Artificial Intelligence and Machine Learning</h2><p>
+	This covered various search algorithms, propositional logic, machine learning and clustering using k-means, and an introduction to neural networks. The assessment for this module was an exam.</p>
+		
+	<h2>Module 6 - Software Engineering</h2><p>
+	Coming soon...</p>
 @endsection
