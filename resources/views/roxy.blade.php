c992f70873e216cf1c757be4c45c4fe1ad640fd9
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2022-07-14 21:21:12 +0100

    Update roxy.blade.php

--- resources/views/roxy.blade.php
+++ resources/views/roxy.blade.php
@@ -3,7 +3,7 @@
 @section ('content')
     <h1>Roxy</h1>
     <p>
-        Roxy is my Cockapoo she is     
+        Roxy is my Cockapoo. She is     
     @php
     //Date format Y-m-d
 $dateOfBirth = '2022-03-18';
@@ -11,6 +11,7 @@ $dateOfBirth = '2022-03-18';
 $age = \Carbon\Carbon::parse($dateOfBirth)->diff(\Carbon\Carbon::now())->format('%y years, %m months and %d days');
 echo $age;
 @endphp
+.
 </p>
     <div id="pixlee_container"></div><script type="text/javascript">window.PixleeAsyncInit = function() {Pixlee.init({apiKey:'kGOzzqFPgdihG5l4l4sP'});Pixlee.addSimpleWidget({widgetId:'36725'});};</script><script src="//instafeed.assets.pxlecdn.com/assets/pixlee_widget_1_0_0.js"></script>
 @endsection
