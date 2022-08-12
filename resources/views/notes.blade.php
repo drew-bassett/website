74ce757c44aca491c9d4bb5235d297a98a10bea6
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2022-08-12 09:50:50 +0100

    Create notes.blade.php

---
+++ resources/views/notes.blade.php
@@ -0,0 +1,6 @@
+@extends ('layout')
+@section ('content')
+<meta name="robots" content="noindex" />
+<h2>Endpoint Manager - Firewall</h2>
+To add exceptions: devices - Windows - configuration profiles - firewall profile should have firewall rules on configuration settings.
+@endsection
