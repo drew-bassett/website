f45c29f25e2ded0b66e492b5ac59f230c7d4286a
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2022-09-03 13:20:06 +0100

    Update roxyandluna.blade.php

--- resources/views/roxyandluna.blade.php
+++ resources/views/roxyandluna.blade.php
@@ -15,10 +15,10 @@ echo $age;
     @php
     //Date format Y-m-d
 $dateOfBirth = '2022-07-02';
-.
 $age = \Carbon\Carbon::parse($dateOfBirth)->diff(\Carbon\Carbon::now())->format('%y years, %m months and %d days');
 echo $age;
 @endphp
+.
 </p>
     <div id="pixlee_container"></div><script type="text/javascript">window.PixleeAsyncInit = function() {Pixlee.init({apiKey:'kGOzzqFPgdihG5l4l4sP'});Pixlee.addSimpleWidget({widgetId:'36725'});};</script><script src="//instafeed.assets.pxlecdn.com/assets/pixlee_widget_1_0_0.js"></script>
 @endsection
