b2e4926e5965fca6bf1adfd201a5b1941aaac009
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2022-10-15 09:40:12 +0100

    Update roxyandluna.blade.php

--- resources/views/roxyandluna.blade.php
+++ resources/views/roxyandluna.blade.php
@@ -3,7 +3,7 @@
 @section ('content')
     <h1>Roxy and Luna</h1>
     <p>
-        Roxy and Luna are my Cockapoos. Roxy is the brown one and is     
+        Roxy and Luna are my Cockapoos. Roxy is the bigger one and is     
     @php
     //Date format Y-m-d
 $dateOfBirth = '2022-03-18';
@@ -11,7 +11,7 @@ $dateOfBirth = '2022-03-18';
 $age = \Carbon\Carbon::parse($dateOfBirth)->diff(\Carbon\Carbon::now())->format('%y years, %m months and %d days');
 echo $age;
 @endphp
-. Luna is the dark one and is 
+. Luna is the smaller one and is 
     @php
     //Date format Y-m-d
 $dateOfBirth = '2022-07-02';
@@ -20,5 +20,5 @@ echo $age;
 @endphp
 .
 </p>
-    <div id="pixlee_container"></div><script type="text/javascript">window.PixleeAsyncInit = function() {Pixlee.init({apiKey:'kGOzzqFPgdihG5l4l4sP'});Pixlee.addSimpleWidget({widgetId:'36725'});};</script><script src="//instafeed.assets.pxlecdn.com/assets/pixlee_widget_1_0_0.js"></script>
+    <img src="/images/roxy_and_luna.jpg alt="Roxy and Luna" width="50%" height="50%"><br/>
 @endsection
