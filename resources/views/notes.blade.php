b18579bba84d21a0790cd4a28f1afc3fe6200ea9
Drew Baxter <75068256+missdrewbaxter@users.noreply.github.com>
2023-01-02 19:42:27 +0000

    Delete notes.blade.php

--- resources/views/notes.blade.php
+++
@@ -1,9 +0,0 @@
-@extends ('layout')
-@section ('content')
-<meta name="robots" content="noindex" />
-<h2>Endpoint Manager - Firewall</h2>
-To add exceptions: devices - Windows - configuration profiles - firewall profile should have firewall rules on configuration settings.
-<h2>Powershell - Update Document Dates</h2>
-(Get-Item "filename").CreationTime=("17 August 2022 11:09:00")<br/>
-(Get-Item "filename").LastWriteTime=("17 August 2022 11:09:00")
-@endsection
